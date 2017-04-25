8aa09700e6a5b1a1673914230147b32681098663
Lucas de Aguilar <lucasdeaguilarjc@hotmail.com>
2017-04-25 12:24:41 -0300

    Aula de terça

--- starwars.js
+++ starwars.js
@@ -3,3 +3,24 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+
+let filmes = [];
+let moviesnav = document.querySelector('#movies');
+
+$.getJSON('http://swapi.co/api/films/', function(data) {
+	  let i = 0;
+	  filmes = data;
+	  console.log(filmes.results[0]);
+	  console.log(moviesnav.childNodes);
+	  console.log(moviesnav.childNodes[1].firstElementChild);
+	  
+	  for (let resultado of filmes.results) {
+		  moviesnav.childNodes[1].children[i].innerHTML=resultado.title;
+		  i++;
+	  }
+	  
+	  //moviesnav.nextSibling.childNodes[0] = filmes.results[4].title;
+	  //moviesnav.innerHTML = '<li data-episode-url="'+filmes.results[5].title+'">Episode I</li>';
+//moviesnav.insertBefore(HTMLNovo, moviesnav.firstChild.nextSibling);
+})
+	
